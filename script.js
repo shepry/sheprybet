308c650becc37bd33dd6d1bea0cc8a3c9a60cdb7
shepry <179221033+shepry@users.noreply.github.com>
2024-08-26 13:24:23 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const teamsList = document.getElementById('teams-list');
+    teamsList.innerHTML = '<p>List of EPL teams will go here.</p>';
+});
